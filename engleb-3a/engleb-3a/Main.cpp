7c72023f82069c62aac22e7c3c6322c6c1cb4912
David <bavib26@gmail.com>
2014-10-21 16:39:30 -0400

    added find matches

--- engleb-3a/engleb-3a/Main.cpp
+++ engleb-3a/engleb-3a/Main.cpp
@@ -12,3 +12,28 @@ void main(){
 
 	cout << "\nDone.\t";
 }
+
+void findMatches(dictionary dict, grid grid){
+	for (int i = 0; i < dict.list.size; i++){										//iterate through every word in dictionary
+		for (int l = 0; l < grid.getLength(); i++){									//cover matrix length
+			for (int w = 0; w < grid.getWidth(); i++){								//cover matrix width
+				if (dict.list[i][0] == grid.workGrid[w][l]){						//check if first letter matches
+					for (int x = -1; x <= 1; x++){									//x and y represent all directions
+						for (int y = -1; y <= 1; y++){
+							if (y == 0 && x == 0)
+								continue;
+							for (int j = 0; j < dict.list[i].length; j++){			//final loop for checking for matches
+								if (j == dict.list[i].length){
+									cout << dict.list[i];
+									break;
+								}
+								if (dict.list[i][j] != grid.workGrid[w + j * y][l + j * x])
+									break;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
