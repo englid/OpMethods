071d6d547a3de11f9c2ffe2818d0c5a9a0a67b2a
David English <bavib26@gmail.com>
2014-10-22 11:09:39 -0400

    search works

--- engleb-3a/engleb-3a/Main.cpp
+++ engleb-3a/engleb-3a/Main.cpp
@@ -8,33 +8,46 @@ void findMatches(dictionary dict, grid grid);
 void testSearch(string dictFile);
 
 void main(){
-	dictionary test = dictionary("dictionary.txt");
+	dictionary test = dictionary("test.txt");
 	grid test2("input15.txt", 15);
-	//findMatches(test, test2);
+	findMatches(test, test2);
 	test2.printGrid();
 
 	cout << endl << endl;
 	grid test3("input30.txt", 30);
-	test3.printGrid();
+	//test3.printGrid();
 
 	cout << "\nDone.\t";
+	int f;
+	cin >> f;
 }
 
 void findMatches(dictionary dict, grid grid){
+	int n, m;
 	for (int i = 0; i < dict.list.size(); i++){										//iterate through every word in dictionary
-		for (int l = 0; l < grid.getSize(); i++){									//cover matrix length
-			for (int w = 0; w < grid.getSize(); i++){								//cover matrix width
-				if (dict.list[i][0] == grid.workGrid[w][l]){						//check if first letter matches
+		for (int l = 0; l < grid.getSize(); l++){									//cover matrix length
+			for (int w = 0; w < grid.getSize(); w++){								//cover matrix width
+				if ((dict.list[i])[0] == grid.workGrid[w][l]){						//check if first letter matches
 					for (int x = -1; x <= 1; x++){									//x and y represent all directions  <-- Not sure how the algorithm works at this point -Rob
 						for (int y = -1; y <= 1; y++){
 							if (y == 0 && x == 0)
 								continue;
 							for (int j = 0; j < dict.list[i].length(); j++){			//final loop for checking for matches
-								if (j == dict.list[i].length()){
-									cout << dict.list[i];
+								if (j == dict.list[i].length()-1){
+									cout << dict.list[i]<< endl;
 									break;
 								}
-								if (dict.list[i][j] != grid.workGrid[w + j * y][l + j * x])
+								n = (w + j * y);
+								m = (l + j * x);
+								if (n < 0)
+									n = n + grid.getSize();
+								if (n > grid.getSize())
+									n = n - grid.getSize();
+								if (m < 0)
+									m = m + grid.getSize();
+								if (m > grid.getSize())
+									m = m - grid.getSize();
+								if ((dict.list[i])[j] != grid.workGrid[n][m])
 									break;
 							}
 						}
