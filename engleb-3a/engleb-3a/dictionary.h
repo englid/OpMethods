7ec41089ba5de38c258c4d3fe36f280d3fb8457b
David English <bavib26@gmail.com>
2014-10-30 10:26:03 -0400

    added binary search

--- engleb-3a/engleb-3a/dictionary.h
+++ engleb-3a/engleb-3a/dictionary.h
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int partition(int f, int l, string list[]);
+
 
 class dictionary{
 private:
@@ -15,7 +15,9 @@ public:
 	void selectionSort();
 	void quickSort();
 	void quickSort(int l, int f, vector<string> temp);
+	int partition(int f, int l, vector<string> temp);
 	void swap(int i, int m);
+	int dictionary::binarySearchByLetter(char word, vector<string> dictionary, int bottom, int top);
 	void print();
 };
 
@@ -58,18 +60,19 @@ void dictionary::print(){
 }
 
 void dictionary::quickSort(){
-	quickSort(0, list.size() - 1, list);
+	this->quickSort(0, list.size() - 1, list);
 }
 
-int partition(int f, int l, vector<string> temp){
+int dictionary::partition(int f, int l, vector<string> temp){
 	int pivotIndex = f;
 	for (int i = f + 1; i <= l; i++){
 		if (temp[i].compare(temp[pivotIndex]) < 0){
 			pivotIndex++;
-			swap(i, pivotIndex);
+			if (i != pivotIndex)
+				swap(i, pivotIndex);
 		}
 	}
-	swap(f, pivotIndex);
+	if (f != pivotIndex){ swap(f, pivotIndex); }
 	return pivotIndex;
 }
 
@@ -79,4 +82,26 @@ void dictionary::quickSort(int f, int l, vector<string> temp){
 		quickSort(f, pivotIndex - 1, temp);
 		quickSort(pivotIndex + 1, l, temp);
 	}
+}
+
+int dictionary::binarySearchByLetter(char word, vector<string> dictionary, int bottom, int top)  //returns -1 if no letter is found, else returns index where letter is found
+{
+	if (bottom > top)
+	{
+		return -1;
+	}
+	int middle = (bottom + top) / 2;
+	if (word == list[middle][0])
+	{
+		return middle;
+	}
+	else if (word > list[middle][0])
+	{
+		return binarySearchByLetter(word, list, middle + 1, top);
+	}
+	else if (word < list[middle][0])
+	{
+		return binarySearchByLetter(word, list, bottom, middle - 1);
+	}
+	return -1;
 }
